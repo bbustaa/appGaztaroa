8875c8ef86920a4d81ae3f034f6605ef50f37a31
Valentina Bustamante <bustamante.141685@e.unavarra.es>
2026-03-24 17:21:36 +0100

    Primeros pasos en React Native

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>oh oh valentina</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bde0fe',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#1d3557',
   },
 });
